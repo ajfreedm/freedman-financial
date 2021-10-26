03c40747d1614f20d3e6e2ba93a98ac1dc34ca26
Andrew Freedman <ajfreedm@gmail.com>
2021-10-26 13:17:49 -0400

    started adding axios to account

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,14 +1,35 @@
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+const url ="https://api.airtable.com/v0/appK4WDNdIvyVcUAu/Table%201?api_key=keygMy5j7EsXv29IM&filterByFormula=({username}='johndoe')"
 function Account() {
-    return (
-      <main>
 
-      <section id="account-wrapper">
-        <div id = "account">-555</div>
-        <div id="balance">$10,000</div>
-      </section>
+  
 
+    const [account, setAccount] = useState( [] );
+  
+    useEffect(() => {
+      const axiosAccount = async () => {
+        const response = await axios(url);
+        setAccount(response.data);
+      };
+        console.log(JSON.stringify(account));
+      axiosAccount();
+    }, []);
+
+
+    
+
+  return (
+    <main>
+      <div id="accountContainer">
+        <div id="account-wrap">
+          <div id="account-number">Account Number: -555</div>
+          <div id="account-ballance">Balance: $10,000</div>
+        </div>
+      </div>
     </main>
-    )
+  );
 }
 
-export default Account
+export default Account;
